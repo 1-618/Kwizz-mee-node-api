559a4234416c8f40e34cec44d9d2261c078a3f58
Aryscene <aryscene@gmail.com>
2021-09-14 12:54:13 +0200

    first commit

--- source/config/config.ts
+++ source/config/config.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 8083;
+const SERVER_PORT = process.env.PORT || 8083;
 
 
 const config = {
